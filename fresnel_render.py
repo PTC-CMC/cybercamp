56f6911269d3c85e6d85ec98446d372eea6fc2eb
Christopher Iacovella <cri@MB22.local>
2022-05-27 13:31:48 -0700

    forgot to git add the fresnel_render.py file

---
+++ fresnel_render.py
@@ -0,0 +1,83 @@
+import warnings
+
+import fresnel
+import IPython
+import packaging.version
+import math
+import itertools
+import PIL
+import numpy
+import io
+
+
+device = fresnel.Device()
+tracer = fresnel.tracer.Path(device=device, w=300, h=300)
+
+FRESNEL_MIN_VERSION = packaging.version.parse("0.13.0")
+FRESNEL_MAX_VERSION = packaging.version.parse("0.14.0")
+
+
+def render(snapshot):
+    if ('version' not in dir(fresnel) or packaging.version.parse(
+            fresnel.version.version) < FRESNEL_MIN_VERSION
+            or packaging.version.parse(
+                fresnel.version.version) >= FRESNEL_MAX_VERSION):
+        warnings.warn(
+            f"Unsupported fresnel version {fresnel.version.version} - expect errors."
+        )
+    L = snapshot.configuration.box[0]
+    scene = fresnel.Scene(device)
+    geometry = fresnel.geometry.Sphere(scene,
+                                       N=len(snapshot.particles.position),
+                                       radius=0.5)
+    geometry.material = fresnel.material.Material(color=fresnel.color.linear(
+        [252 / 255, 209 / 255, 1 / 255]),
+                                                  roughness=0.5)
+    geometry.position[:] = snapshot.particles.position[:]
+    geometry.outline_width = 0.04
+    box = fresnel.geometry.Box(scene, [L, L, L, 0, 0, 0], box_radius=.02)
+
+    scene.lights = [
+        fresnel.light.Light(direction=(0, 0, 1),
+                            color=(0.8, 0.8, 0.8),
+                            theta=math.pi),
+        fresnel.light.Light(direction=(1, 1, 1),
+                            color=(1.1, 1.1, 1.1),
+                            theta=math.pi / 3)
+    ]
+    scene.camera = fresnel.camera.Orthographic(position=(L * 2, L, L * 2),
+                                               look_at=(0, 0, 0),
+                                               up=(0, 1, 0),
+                                               height=L * 1.4 + 1)
+    scene.background_color = (1, 1, 1)
+    return tracer.sample(scene, samples=100)
+
+    #return IPython.display.Image(tracer.sample(scene, samples=100)._repr_png_())
+
+
+def render_movie(frames):
+
+    a = render(frames[0])
+
+    im0 = PIL.Image.fromarray(a[:, :, 0:3],
+                              mode='RGB').convert("P",
+                                                  palette=PIL.Image.ADAPTIVE)
+    ims = []
+    for i, f in enumerate(frames[1:]):
+        a = render(f)
+        im = PIL.Image.fromarray(a[:, :, 0:3], mode='RGB')
+        im_p = im.quantize(palette=im0)
+        ims.append(im_p)
+
+    blank = numpy.ones(shape=(im.height, im.width, 3), dtype=numpy.uint8) * 255
+    im = PIL.Image.fromarray(blank, mode='RGB')
+    im_p = im.quantize(palette=im0)
+    ims.append(im_p)
+
+    f = io.BytesIO()
+    im0.save(f, 'gif', save_all=True, append_images=ims, duration=1000, loop=0)
+
+    size = len(f.getbuffer()) / 1024
+    if (size > 3000):
+        warnings.warn(f"Large GIF: {size} KiB")
+    return IPython.display.display(IPython.display.Image(data=f.getvalue()))
